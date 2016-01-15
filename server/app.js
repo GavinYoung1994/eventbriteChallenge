b12b604b957363cf872af4e22ace895d6048858a
Gavin Young <gavinyoung1994@gmail.com>
2016-01-15 02:08:11 -0500

    done

---
+++ server/app.js
@@ -0,0 +1,42 @@
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var request = require('request');
+
+var app = express();
+module.exports = app;
+
+// The path of our public directory. ([ROOT]/public)
+var publicPath = path.join(__dirname, '../public');
+
+// The path of our index.html file. ([ROOT]/index.html)
+var indexHtmlPath = path.join(__dirname, '../index.html');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use(express.static(publicPath));
+
+app.get('/', function (req, res) {
+    res.sendFile(indexHtmlPath);
+});
+
+
+app.get('/data/:id', function (req, res){
+    request({
+      url: 'https://www.eventbriteapi.com/v3/events/search/?popular=on&token=NDLRP3PVI4DELH76QP2Y&page='+req.params.id,
+      method: 'GET'
+    },
+    function(err, response, body) {
+      if(err || response.statusCode != 200) {
+        callback('Unable to authenticate with Eventbrite.');
+        return;
+      } else {
+        var js = JSON.parse(body);
+        res.send(js.events);
+      }
+  });
+})
+
+
+
